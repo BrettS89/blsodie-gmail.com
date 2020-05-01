9a3bb8fcc3661cff354459f9b174db88ccbfeb12
BrettS89 <blsodie@gmail.com>
2020-04-30 21:12:36 -0400

    adding tc and privacy policy

--- src/components/PrivacyPolicy/styles.js
+++ src/components/PrivacyPolicy/styles.js
@@ -4,5 +4,13 @@ export default StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 15,
+  },
+  text: {
+    flexWrap: 'wrap',
+  },
+  mText: {
+    flexWrap: 'wrap',
+    marginBottom: 10,
   },
 });
